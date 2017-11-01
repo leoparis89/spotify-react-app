9e2387076356cd9d1082f76585a73e123f44b648
Lev Kowalski <levkowalski@mbp-de-lev.home>
2017-11-02 00:51:23 +0100

    chore: refine webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: 'public'
+        contentBase: 'public',
+        historyApiFallback: true,
     },
     module: {
         loaders: [
