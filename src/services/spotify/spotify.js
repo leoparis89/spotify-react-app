0399322633040973065c51cd41bbaa5f2d0f9e11
Lev Kowalski <levkowalski@mbp-de-lev.home>
2017-11-04 17:28:37 +0100

    feat(artistCard): put image url logic in service

--- src/services/spotify/spotify.js
+++ src/services/spotify/spotify.js
@@ -3,14 +3,17 @@ import { storage } from '../storage/storage';
 import { authHttp } from '../authHttp/authHttp';
 
 class SpotifyService {
-    searchArtist(artistName, offset=0) {
+    searchArtist(artistName, offset = 0) {
         let searchParams = new URLSearchParams();
-        searchParams.append('q', artistName)
-        searchParams.append('type', 'artist')
-        searchParams.append('offset', offset)
+        searchParams.append('q', artistName);
+        searchParams.append('type', 'artist');
+        searchParams.append('offset', offset);
         return authHttp.get('https://api.spotify.com/v1/search?' + searchParams.toString())
             .then((res) => {
-                return res.data.artists.items;
+                return res.data.artists.items.map(artist => {
+                    artist.image = artist.images[0] && artist.images[0].url;
+                    return artist;
+                });
             });
     }
 }
