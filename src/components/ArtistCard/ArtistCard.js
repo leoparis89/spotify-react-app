4b4f05f686195fc1ba39287ad88f217ad8950b3b
Lev Kowalski <levkowalski@mbp-de-lev.home>
2017-11-04 17:29:16 +0100

    feat(artistCard): use propTypes and display music styles

--- src/components/ArtistCard/ArtistCard.js
+++ src/components/ArtistCard/ArtistCard.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ArtistCard = (props) => {
-  return (
-    <div className="col-md-3">
-      <h3>
-      {props.artist.name}
-      </h3>
-      {props.artist.images[0] ? 
-        <img src={props.artist.images[0].url}
-          className="img-responsive"
-         alt={props.artist.name} />
-        : '' }
-    </div>
-  );
-}
+
+    return (
+        <div className="col-md-3">
+            <h3>
+                {props.artist.name}
+            </h3>
+            <img src={props.artist.image}
+                className="img-responsive"
+                alt={props.artist.name} />
+            <div>
+                {props.artist.genres.map(genre => {
+                    return <span className="badge">{genre}</span>;
+                })}
+            </div>
+
+        </div>
+    );
+};
+
+ArtistCard.propTypes = {
+    artist: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        image: PropTypes.string,
+        genres: PropTypes.array
+    })
+};
 
 export default ArtistCard;
