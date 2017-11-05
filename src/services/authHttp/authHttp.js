f904c6029e50f3d0e94bc9cfcbe6538b618dafbc
Lev Kowalski <levkowalski@mbp-de-lev.home>
2017-11-05 13:14:41 +0100

    refactor(authHttp): use functions

--- src/services/authHttp/authHttp.js
+++ src/services/authHttp/authHttp.js
@@ -1,14 +1,9 @@
 import axios from 'axios';
 import { storage } from '../storage/storage';
 
-class AuthHttp {
-
-    get(url) {
-        let config = {
-            headers: { 'Authorization': 'Bearer ' + storage.getToken() }
-        };
-        return axios.get(url, config);
-    }
-}
-
-export const authHttp = new AuthHttp();
+export const authGet = (url) => {
+    const config = {
+        headers: { 'Authorization': 'Bearer ' + storage.getToken() }
+    };
+    return axios.get(url, config);
+};
