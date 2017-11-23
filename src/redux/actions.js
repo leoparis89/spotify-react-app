c4f819f42b9c1ac9ce496a15a5b0a0f0b31f121e
Lev Kowalski <levkowalski@gmail.com>
2017-11-23 17:42:12 +0100

    feat: add actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,23 +3,23 @@
  */
 
 export const SEARCH_ARTISTS = 'SEARCH_ARTISTS';
+export const SEARCH_ARTISTS_COMPLETE = 'SEARCH_ARTISTS_COMPLETE';
+export const SEARCH_ARTISTS_FAILED = 'SEARCH_ARTISTS_FAILED';
 // export const TOGGLE_TODO = 'TOGGLE_TODO';
 // export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
-
-/*
- * other constants
- */
-
-export const VisibilityFilters = {
-    SHOW_ALL: 'SHOW_ALL',
-    SHOW_COMPLETED: 'SHOW_COMPLETED',
-    SHOW_ACTIVE: 'SHOW_ACTIVE'
-};
 
 /*
  * action creators
  */
 
 export function searchArtists(query) {
-    return { type: SEARCH_ARTISTS, query };
+  return { type: SEARCH_ARTISTS, query };
+}
+
+export function searchArtistsComplete(artists) {
+  return { type: SEARCH_ARTISTS_COMPLETE, artists };
+}
+
+export function searchArtistsFailed(error) {
+  return { type: SEARCH_ARTISTS_FAILED, error };
 }
