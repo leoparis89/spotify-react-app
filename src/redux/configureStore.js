3c251b5194c7e0523a158b794b61691854c02455
Lev Kowalski <levkowalski@gmail.com>
2017-11-23 16:07:44 +0100

    feat: use configure store per environment

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,25 +1,7 @@
-import { createStore, compose } from 'redux';
-import rootReducer from './reducers';
-import DevTools from '../containers/DevTools';
-
-const enhancer = compose(
-    // Middleware you want to use in development:
-    // applyMiddleware(d1, d2, d3),
-    // Required! Enable Redux DevTools with the monitors you chose
-    DevTools.instrument()
-);
-
-export default function configureStore(initialState) {
-    // Note: only Redux >= 3.1.0 supports passing enhancer as third argument.
-    // See https://github.com/reactjs/redux/releases/tag/v3.1.0
-    const store = createStore(rootReducer, initialState, enhancer);
-
-    // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
-    if (module.hot) {
-        module.hot.accept('./reducers', () =>
-            store.replaceReducer(require('./reducers')/*.default if you use Babel 6+ */)
-        );
-    }
-
-    return store;
+// Use DefinePlugin (Webpack) or loose-envify (Browserify)
+// together with Uglify to strip the dev branch in prod build.
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod');
+} else {
+  module.exports = require('./configureStore.dev');
 }
