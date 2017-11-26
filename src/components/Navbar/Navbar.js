7b613004bfa9e25528927b42d9bf51c0e652e03c
Lev Kowalski <levkowalski@mbp-de-lev.home>
2017-11-26 23:14:36 +0100

    feat: add link to search page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,32 @@
 
 import React from 'react';
 import { authentication } from '../../services/authentication/authentication';
+import {Link} from 'react-router-dom';
 
 class Navbar extends React.Component {
     state = {}
 
     logout = () => {
-        authentication.logout();
+      authentication.logout();
     }
 
     render() {
-        return (
-            <nav className="navbar navbar-inverse">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <a className="navbar-brand" href="#">Spotify App</a>
-                    </div>
-                    <ul className="nav navbar-nav">
-                        <li className="active"><a href="#">Home</a></li>
-                        <li><a href="" onClick={this.logout}>Logout</a></li>
-                    </ul>
-                </div>
-            </nav>
-        );
+      return (
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">Spotify App</a>
+            </div>
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#">Home</a></li>
+              <li className="active">
+                <Link to="/search" style={{ color: 'white' }} activeStyle={{ color: 'red' }}>Search</Link>
+              </li>
+              <li><a href="" onClick={this.logout}>Logout</a></li>
+            </ul>
+          </div>
+        </nav>
+      );
     }
 }
 
