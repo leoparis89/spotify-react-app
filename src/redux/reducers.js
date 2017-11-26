17a39c88ff20901e1c4b7d0ca3a8a0eeba246ade
Lev Kowalski <levkowalski@mbp-de-lev.home>
2017-11-26 22:00:13 +0100

    feat: add loading state to reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,20 +1,28 @@
 import {
-  SET_QUERY, SEARCH_ARTISTS_COMPLETE
+  SET_QUERY, SEARCH_ARTISTS_START, SEARCH_ARTISTS_COMPLETE
 } from './actions';
 import { combineReducers } from 'redux';
 
 const initialState = {
   results: [],
   query: null,
-  offset: 0
+  offset: 0,
+  loading: false
 };
 
-function search(state = initialState  , action) {
+function search(state = initialState, action) {
   switch (action.type) {
   case SET_QUERY:
     return { query: action.query, offset: 0, results: [] };
+  case SEARCH_ARTISTS_START:
+    return { ...state, loading: true };
   case SEARCH_ARTISTS_COMPLETE:
-    return {...state, offset: state.offset + 20, results: state.results.concat(action.results)};
+    return {
+      ...state,
+      loading: false,
+      offset: state.offset + 20,
+      results: state.results.concat(action.results)
+    };
   default:
     return state;
   }
