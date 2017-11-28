3ae220bae94fcfa3c26117ee184b1f701b786160
Lev Kowalski <levkowalski@mbp-de-lev.home>
2017-11-28 23:22:40 +0100

    feat: display genres and center cover

--- src/components/ArtistProfile/ArtistProfile.js
+++ src/components/ArtistProfile/ArtistProfile.js
@@ -3,18 +3,27 @@ import React from 'react';
 const ArtistProfile = (props) => {
 
   var divStyle = {
-    backgroundImage: 'url(' + props.artist.image + ')'
+    backgroundImage: 'url(' + props.artist.image + ')',
+    backgroundSize: 'cover'
     // WebkitTransition: 'all', // note the capital 'W' here
     // msTransition: 'all' // 'ms' is the only lowercase vendor prefix
   };
 
-
+  const { artist } = props;
   return (
+
+
     <div>
       <div className="jumbotron"
         style={divStyle}
-      ><h1>{props.artist.name}</h1>
+      ><h1>{artist.name}</h1>
       </div>
+      {artist.genres.map(genre => {
+        return (<span
+          key={genre}
+          className="label label-info"
+        >{genre}</span>);
+      })}
     </div >
   );
 };
