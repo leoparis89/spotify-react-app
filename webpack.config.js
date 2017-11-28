e493540bcc8b6a46f98bbe0b885b3fea9d5dd2bd
Lev Kowalski <levkowalski@gmail.com>
2017-11-28 15:45:57 +0100

    feat: add hot reload config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,33 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/Index.js',
-    output: {
-        path: path.resolve('build/js'),
-        publicPath: '/js',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: 'public',
-        historyApiFallback: true,
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/, 
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2)$/,
-                loader: 'file-loader'
-            },
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-        ]
-    }
+  entry: {
+    'app': [
+      'babel-polyfill',
+      'react-hot-loader/patch',
+      './src/Index'
+    ]
+  },
+  output: {
+    path: path.resolve('build/js'),
+    publicPath: '/js',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: 'public',
+    historyApiFallback: true,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader'
+      },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  }
 };
