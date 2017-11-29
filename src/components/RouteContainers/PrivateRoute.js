65e140adb3eb6983e49a640f27b0fa9ff94e75f3
Lev Kowalski <levkowalski@gmail.com>
2017-11-29 16:10:41 +0100

    feat: refactor things

--- src/components/RouteContainers/PrivateRoute.js
+++ src/components/RouteContainers/PrivateRoute.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Redirect } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 
-function PrivateRoute ({component: Component, authed, ...rest}) {
-    return (
-      <Route
-        {...rest}
-        render={(props) => authed === true
-          ? <div><Navbar /><Component {...props} /></div>
-          : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
-      />
-    )
-  }
+function PrivateRoute({ component: Component, authed, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        authed ?
+          <div><Navbar /><Component {...props} /></div>
+          :
+          <Redirect to={{ pathname: '/login', state: { from: props.location } }} />}
+    />
+  );
+}
 
-  export default PrivateRoute;
+export default PrivateRoute;
--- src/components/RouteContainers/PublicRoute.js
+++ src/components/RouteContainers/PublicRoute.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Redirect } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
-function PublicRoute({ component: Component, authed,loginSuccessfull, ...rest }) {
+function PublicRoute({ component: Component, authed, loginSuccessfull, ...rest }) {
   return (
     <Route
       {...rest}
-      render={(props) => authed === false
-        ? <Component {...props} loginSuccessfull={loginSuccessfull} />
-        : <Redirect to={{ pathname: '/search', state: { from: props.location } }} />}
+      render={(props) =>
+        !authed ?
+          <Component {...props} loginSuccessfull={loginSuccessfull} />
+          :
+          <Redirect to={{ pathname: '/search', state: { from: props.location } }} />}
     />
-  )
+  );
 }
 
 export default PublicRoute;
