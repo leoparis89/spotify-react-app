3c7be82f1a4cf265482597bf1aa2bd4d4c4af52a
Lev Kowalski <levkowalski@gmail.com>
2017-11-30 14:01:38 +0100

    refactor: create auth containers

--- src/components/RouteContainers/PrivateRoute.js
+++ src/components/RouteContainers/PrivateRoute.js
@@ -1,18 +1,79 @@
+// import React from 'react';
+// import { Route, Redirect } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
+// import { connect } from 'react-redux';
+
+// function PrivateRoute({ component: Component, authed, ...rest }) {
+//   return (
+//     <Route
+//       {...rest}
+//       render={(props) =>
+//         authed ?
+//           <div><Navbar /><Component {...props} /></div>
+//           :
+//           <Redirect to={{ pathname: '/login', state: { from: props.location } }} />}
+//     />
+//   );
+// }
+
+// export default PrivateRoute;
+
+
+
+
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import Navbar from '../Navbar/Navbar';
+import { connect } from 'react-redux';
 
-function PrivateRoute({ component: Component, authed, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        authed ?
-          <div><Navbar /><Component {...props} /></div>
-          :
-          <Redirect to={{ pathname: '/login', state: { from: props.location } }} />}
-    />
-  );
+
+const mapStateToProps = (state, ownProps) => {
+  
+  return {
+    isloggedIn: state.session.isLoggedIn
+  };  
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+  };
+};
+
+class PrivateRoute extends React.Component {
+  render() {
+    
+    // const { isloggedIn } = this.state;
+    const { component: Component, isloggedIn } = this.props;
+    return (
+      <Route
+        render={(props) =>
+          isloggedIn ?
+            <div><Navbar /><Component {...props} /></div>
+            :
+            <Redirect to={{ pathname: '/login', state: { from: props.location } }} />}
+      />
+    );
+  }
 }
 
-export default PrivateRoute;
+// export default;
+
+
+
+// function PublicRoute({ component: Component, authed, loginSuccessfull, ...rest }) {
+//   return (
+//     <Route
+//       {...rest}
+//       render={(props) =>
+//         !authed ?
+//           <Component {...props} loginSuccessfull={loginSuccessfull} />
+//           :
+//           <Redirect to={{ pathname: '/search', state: { from: props.location } }} />}
+//     />
+//   );
+// }
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PrivateRoute);
--- src/components/RouteContainers/PublicRoute.js
+++ src/components/RouteContainers/PublicRoute.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-function PublicRoute({ component: Component, authed, loginSuccessfull, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        !authed ?
-          <Component {...props} loginSuccessfull={loginSuccessfull} />
-          :
-          <Redirect to={{ pathname: '/search', state: { from: props.location } }} />}
-    />
-  );
+
+const mapStateToProps = (state, ownProps) => {
+  
+  return {
+    isloggedIn: state.session.isLoggedIn
+  };  
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+  };
+};
+
+class PublicRoute extends React.Component {
+  render() {
+    
+    // const { isloggedIn } = this.state;
+    const { component: Component, isloggedIn } = this.props;
+    return (
+      <Route
+        render={(props) =>
+          !isloggedIn ?
+            <Component {...props} />
+            :
+            <Redirect to={{ pathname: '/search', state: { from: props.location } }} />}
+      />
+    );
+  }
 }
 
-export default PublicRoute;
+// export default;
+
+
+
+// function PublicRoute({ component: Component, authed, loginSuccessfull, ...rest }) {
+//   return (
+//     <Route
+//       {...rest}
+//       render={(props) =>
+//         !authed ?
+//           <Component {...props} loginSuccessfull={loginSuccessfull} />
+//           :
+//           <Redirect to={{ pathname: '/search', state: { from: props.location } }} />}
+//     />
+//   );
+// }
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PublicRoute);
