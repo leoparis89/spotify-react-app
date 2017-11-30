b42ccbacbe6074a0b089f7d0ddd6d06e85797556
Lev Kowalski <levkowalski@gmail.com>
2017-11-30 16:36:33 +0100

    feat: connect navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,33 +2,42 @@
 import React from 'react';
 import { authentication } from '../../services/authentication/authentication';
 import {Link} from 'react-router-dom';
-
-class Navbar extends React.Component {
-    state = {}
-
-    logout = () => {
-      authentication.logout();
-    }
-
-    render() {
-      return (
-        <nav className="navbar navbar-inverse">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <a className="navbar-brand" href="#">Spotify App</a>
-            </div>
-            <ul className="nav navbar-nav">
-              <li className="active"><a href="#">Home</a></li>
-              <li className="active">
-                <Link to="/search" style={{ color: 'white' }} activeStyle={{ color: 'red' }}>Search</Link>
-              </li>
-              <li><a href="" onClick={this.logout}>Logout</a></li>
-            </ul>
-          </div>
-        </nav>
-      );
-    }
-}
+import { connect } from 'react-redux';
 
 
-export default Navbar;
+const mapStateToProps = (state, ownProps) => {
+  const {id} = state.profile;
+  return { id };  
+};
+
+const Navbar = (props) => {
+  const {id} = props;
+  return (
+    <nav className="navbar navbar-inverse">
+      <div className="container">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="#">Spotify App</a>
+        </div>
+        <ul className="nav navbar-nav">
+          <li className="active"><a href="#">Home</a></li>
+          <li className="active">
+            <Link to="/search" style={{ color: 'white' }} activeStyle={{ color: 'red' }}>Search</Link>
+          </li>
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+        <li className="active"><a href="#">{id}</a></li>
+          <li><a href="#"><span className="glyphicon glyphicon-user"></span></a></li>  
+          <li><a href=""
+            onClick={this.logout}
+          ><span className="glyphicon glyphicon-off"
+            ></span></a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+
+export default connect(
+  mapStateToProps
+)(Navbar);
