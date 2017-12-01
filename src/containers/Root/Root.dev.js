ea5f981fba75f859efb3997e0f82bdca775644d7
Lev Kowalski <levkowalski@mbp-de-lev.home>
2017-12-01 12:07:25 +0100

    chore: clean up code and remove warning

--- src/containers/Root/Root.dev.js
+++ src/containers/Root/Root.dev.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../../redux/configureStore';
-let store = configureStore({artists: []});
 import DevTools from '../DevTools';
 import App from '../../components/App/App';
-// ;
-
+let store = configureStore();
 
 const Root = () => (
 
--- src/containers/Root/Root.prod.js
+++ src/containers/Root/Root.prod.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../../redux/configureStore';
-let store = configureStore({artists: []});
 import App from '../../components/App/App';
+let store = configureStore();
 
 const Root = () => (
 
