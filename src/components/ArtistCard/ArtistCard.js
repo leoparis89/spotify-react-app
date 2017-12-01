5323a643eb248d175048e009d0682ac740cdece0
Lev Kowalski <levkowalski@mbp-de-lev.home>
2017-12-01 12:10:14 +0100

    chore: fix indent

--- src/components/ArtistCard/ArtistCard.js
+++ src/components/ArtistCard/ArtistCard.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './ArtistCard.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 const ArtistCard = (props) => {
 
     
-    return (
-        <Link to={'artist/' + props.artist.id}>
-        <div className="col-md-3">
-                <h3>
-                    {props.artist.name}
-                </h3>
-                <div className="image">
-                    <img src={props.artist.image}
-                        className="img-responsive"
-                        alt={props.artist.name} />
-                </div>
+  return (
+    <Link to={'artist/' + props.artist.id}>
+      <div className="col-md-3">
+        <h3>
+          {props.artist.name}
+        </h3>
+        <div className="image">
+          <img src={props.artist.image}
+            className="img-responsive"
+            alt={props.artist.name} />
+        </div>
 
-            </div>
-        </Link>
-    );
+      </div>
+    </Link>
+  );
 };
 
 ArtistCard.propTypes = {
-    artist: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        name: PropTypes.string.isRequired,
-        image: PropTypes.string,
-    })
+  artist: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string,
+  })
 };
 
 export default ArtistCard;
