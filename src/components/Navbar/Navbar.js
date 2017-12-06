491afb5a4127bd07c996e6df18055d256bd2ae4d
Lev Kowalski <levkowalski@gmail.com>
2017-12-06 17:28:14 +0100

    refactor with bootstrap 4 navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,28 +4,25 @@ import {Link} from 'react-router-dom';
 const Navbar = (props) => {
   const {id, logout} = props;
   return (
-    <nav className="navbar navbar-inverse">
-      <div className="container">
-        <div className="navbar-header">
-          <a className="navbar-brand" href="#">Spotify App</a>
-        </div>
-        <ul className="nav navbar-nav">
-          <li className="active"><a href="#">Home</a></li>
-          <li className="active">
-            <Link to="/search" style={{ color: 'white' }}>Search</Link>
-          </li>
-        </ul>
-        <ul className="nav navbar-nav navbar-right">
-          <li className="active"><a href="#">{id}</a></li>
-          <li><a href="#"><span className="glyphicon glyphicon-user"></span></a></li>
-          <li><a href="#"
-            onClick={logout}
-          ><span className="glyphicon glyphicon-off"
-            ></span></a></li>
-        </ul>
-      </div>
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">Spotify App</a>
+      <ul className="nav">
+        <li className="nav-item">
+          <Link to="/search"></Link>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#"><Link to="/search" style={{ color: 'white' }}>Search</Link></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">{id}</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="#" onClick={logout}>logout</a>
+        </li>
+      </ul>
     </nav>
   );
 };
+
 
 export default Navbar;
