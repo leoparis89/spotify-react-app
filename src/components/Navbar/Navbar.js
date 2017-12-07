0af8ecb73cdc07cd9f6407dbadb8ddef83b927e0
Lev Kowalski <levkowalski@gmail.com>
2017-12-07 16:41:43 +0100

    feat: display gravatar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import gravatar from 'gravatar';
 
 const Navbar = (props) => {
-  const {id, logout} = props;
+  const {id, logout, email} = props;
+
   return (
     <nav className="navbar navbar-dark bg-dark">
       <a className="navbar-brand" href="#">Spotify App</a>
@@ -15,6 +17,9 @@ const Navbar = (props) => {
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">{id}</a>
+        </li>
+        <li className="nav-item">
+          {email &&  <img src={gravatar.url(email, {s: '50'})}/> }
         </li>
         <li className="nav-item">
           <a className="nav-link " href="#" onClick={logout}>logout</a>
--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -4,8 +4,8 @@ import {logout, stopKeepAlive} from '../../redux/actions/sessionActions';
 
 
 const mapStateToProps = (state, ownProps) => {
-  const {id} = state.profile;
-  return { id };  
+  const {id, email} = state.profile;
+  return { id, email };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
