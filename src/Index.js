b0d23c0fec83613d67984641830d9619d3aeda3e
Lev Kowalski <levkowalski@gmail.com>
2017-12-07 12:09:04 +0100

    remove unwanted code

--- src/Index.js
+++ src/Index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-//import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
-// import Root from './components/Root/Root';
 
-// ReactDOM.render(<Root />, document.getElementById('root'));
 import { AppContainer } from 'react-hot-loader';
 
 import Root from './containers/Root/Root';
@@ -21,9 +18,6 @@ const render = Component => {
   
 render(Root);
 
-
-window.open('http://google.co', "theFrame");
-  
 if (module.hot) {
   module.hot.accept('./containers/Root/Root', () => { render(Root); });
 }
