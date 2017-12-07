d508207eda0945436a9c4a694dd3c75769457dfd
levkowalski <levkowalski@gmail.com>
2017-12-07 21:13:25 +0100

    style: refactor nav menu cosmetics

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,20 +8,17 @@ const Navbar = (props) => {
   return (
     <nav className="navbar navbar-dark bg-dark">
       <a className="navbar-brand" href="#">Spotify App</a>
-      <ul className="nav">
-        <li className="nav-item">
-          <Link to="/search"></Link>
+      <ul class="nav">
+        <li class="nav-item">
+          <a className="nav-link" href="#"><Link to="/search">Search</Link></a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#"><Link to="/search" style={{ color: 'white' }}>Search</Link></a>
-        </li>
-        <li className="nav-item">
+        <li class="nav-item">
           <a className="nav-link" href="#">{id}</a>
         </li>
-        <li className="nav-item">
-          {email &&  <img src={gravatar.url(email, {s: '50'})}/> }
+        <li class="nav-item">
+          {email &&  <img src={gravatar.url(email, {s: '40'})}/> }
         </li>
-        <li className="nav-item">
+        <li class="nav-item">
           <a className="nav-link " href="#" onClick={logout}>logout</a>
         </li>
       </ul>
