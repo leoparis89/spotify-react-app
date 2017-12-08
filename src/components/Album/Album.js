4a5742affa369f701a964d6afbb508480690908d
Lev Kowalski <levkowalski@gmail.com>
2017-12-08 18:06:13 +0100

    display correct album

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -2,9 +2,9 @@ import React from 'react';
 const Artist = (props) => {
 
   const {id, name, release_date, label, album_type, artists, image, tracks} = props;
-  const url = 'https://open.spotify.com/embed?uri=spotify%3Aalbum%3A2rp5riHULWgrXPsDtsp1ir';
+  const url = `https://open.spotify.com/embed?uri=spotify%3Aalbum%3A${id}`;
 
-  // const url = `https://open.spotify.com/embed?uri=spotify%3Aalbum%${id}`;
+  // const url = `https://open.spotify.com/embed?uri=spotify%3Aalbum%${id}`;"4mjfnkJW18g3Txcu2DEpJe"
   return (
     <div>
       <div className="container">
