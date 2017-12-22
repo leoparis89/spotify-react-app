f7527da6d831b60fbba7e3e2b94b01cf71ae0288
levkowalski <levkowalski@gmail.com>
2017-12-22 23:57:44 +0100

    feat: refactor artist card

--- src/components/ArtistCard/ArtistCard.js
+++ src/components/ArtistCard/ArtistCard.js
@@ -2,22 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ArtistCard.scss';
 import {Link} from 'react-router-dom';
-const ArtistCard = (props) => {
 
-  const {name, image, popularity} = props.artist;
+const ArtistCard = ({artist: {name, image, popularity, id}}) => {
   return (
     <div className="col-md-3">
-      <Link to={'artist/' + props.artist.id}>
+      <Link to={'artist/' + id}>
         <div className="card">
           <div className="square-image-container">
             {
               image ?
-                <img className=""
-                  src={image} alt="Card image cap" />
+                <img src={image} alt="Card image cap" />
                 :
                 <i className="fa fa-music" style={{fontSize: '200px'}}></i>
             }
-
           </div>
           <h3 className="card-body">{name}</h3>
           <p><span className="badge badge-pill badge-primary"> Popularity: {popularity}</span></p>
